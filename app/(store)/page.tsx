163d55be47d3b6abff4108ace269bdb9be243662
rickyfbrianto <71504886+rickyfbrianto@users.noreply.github.com>
2024-12-12 10:37:46 +0800

    Penambahan Docker

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import ProductsView from '@/components/ProductsView'
 import { getAllCategories } from '@/sanity/lib/products/getAllCategories'
 import { getAllProducts } from '@/sanity/lib/products/getAllProducts'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -13,7 +14,7 @@ export default async function page() {
     return (
         <div className=''>
             <div className="flex h-[60vh] bg-gradient-to-tr from-slate-400 to-teal-500 mb-10">
-
+                {/* <Image src={}/> */}
             </div>
             <div className="container mx-auto">
                 <Banner />
